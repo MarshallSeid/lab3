f132f3823546309c1027ed611f3f43914d6c5804
vagrant <vagrant@precise32.(none)>
2015-01-23 02:33:06 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -20,6 +20,8 @@ function initializePage() {
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
 	$("#submitBtn").click(updateProject);
+	$("description").click(projectClick);
+
 }
 function updateProject(e) {
    var projectID = $('#project').val();
@@ -30,14 +32,69 @@ function updateProject(e) {
    var newText = $('#description').val();
    $(projectID + " .project-description").text(newText);
 }
+function changeTitle(e) {
+	var project = $(this).find("p").text();
+	var projectID = $('#project-description').val();
+	$(projectID + ".project-description").toggle();
+}
 function projectClick(e) {
 	e.preventDefault();
+
+	//To be used with stretch goal TOGGLE
+	var fileToToggle = $(this).find("p").text();
+	console.log(fileToToggle);
+	if (fileToToggle== "Waiting in Line")
+		{
+			fileToToggle = "Pizza Time, no more line";
+			var header1 = $("#project1");
+			header1.text(fileToToggle);
+		}
+	else if (fileToToggle == "Needfinding")
+	{
+		fileToToggle = "Found my purpose in Life";
+		var header2 = $("#project2");
+		header2.text(fileToToggle);
+	}
+	else if (fileToToggle == "Prototyping")
+	{
+		fileToToggle = "Monotyped";
+		var header3 = $("#project3");
+		header3.text(fileToToggle);
+	}
+	else if (fileToToggle == "Heuristic Evaluation")
+	{
+		fileToToggle = "I don't know what this means";
+		var header4 = $("#project4");
+		header4.text(fileToToggle);
+	}
+	else if (fileToToggle == "Visualization")
+	{
+		var header5 = $("#project5");
+		header5.text("I can see clearly now");
+	}
+	else if (fileToToggle == "Social design")
+	{
+		var header6 = $("#project6");
+		header6.text("Antisocial mesign");
+	}
+	else if (fileToToggle == "Gestural interaction")
+	{
+		var header7 = $("#project7");
+		header7.text("Dang is this google glass");
+	}
+	else if (fileToToggle == "Design tools")
+	{
+		var header8 = $("#project8");
+		header8.text("Hammers and things");
+	}
+
 
 	//In an event listener
 	var projectTitle = $(this).find("p").text();
 	var jumbotronHeader = $(".jumbotron h1");
 	jumbotronHeader.text(projectTitle);
 	var containingProject = $(this).closest("project");
+	//console.log(containingProject);
 	var description = $(containingProject).find(".project-description");
 	if (description.length == 0) { 
 		containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
